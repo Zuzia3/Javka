fe05151f269bc54ff7674c6d8faa525fdb1a9fb9
juliazmarzlik <96787@student.ue.poznan.pl>
2025-05-14 18:40:19 +0200

    Add files via upload

---
+++ nb.java
@@ -0,0 +1,15 @@
+package abc;
+
+public class nb {
+
+	public static void main(String[] args) {
+				int licznik = 0;
+				do {
+					System.out.println("Licznik: " + licznik);
+					licznik++;
+				} while (licznik < 3);
+			}
+
+		}
+
+	
